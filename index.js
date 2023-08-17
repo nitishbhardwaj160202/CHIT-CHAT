e1a70c7441c9031ed51f13bb4c138cffdd4e5d5d
nitishbhardwaj160202 <140200995+nitishbhardwaj160202@users.noreply.github.com>
2023-08-18 00:10:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ io.on("connection", (socket) => {
   });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  } 
+
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
 });
